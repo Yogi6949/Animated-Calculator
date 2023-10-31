768bfe2ea5089484c41a0c1f15c1366c651dc5f6
Yogi6949 <yogeshbakle6949@gmail.com>
2023-10-31 17:21:09 +0530

    Add initial Files

---
+++ main.js
@@ -0,0 +1,48 @@
+// Calculator JS
+
+// console.log("Welcome to javaScript");
+
+let btn = document.getElementsByClassName("btn");
+let display = document.getElementById("display");
+let string = " ";
+
+let box = document.querySelector(".main");
+box.addEventListener("mousemove", (e) => {
+  let Bwidth = box.offsetWidth;
+  let Bheight = box.offsetHeight;
+  let centerX = box.offsetLeft + Bwidth / 2;
+  let centerY = box.offsetTop + Bheight / 2;
+  let eventX = e.clientX - centerX;
+  let eventY = e.clientY - centerY;
+  let rotateX = (20 * eventY) / (Bheight / 2);
+  let rotateY = (-1 * 20 * eventX) / (Bwidth / 2);
+  box.style.transform = `perspective(1500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+});
+
+box.addEventListener("mouseenter", () => {
+  box.style.transform = "all 0.05s ease";
+});
+
+box.addEventListener("mouseleave", () => {
+  box.style.transform = `rotateX(${0}deg) rotateY(${0}deg)`;
+  box.style.transform = "all 0.06s ease";
+});
+
+// Logic
+
+Array.from(btn).forEach((element) => {
+  element.addEventListener("click", (e) => {
+    let val = e.target.innerHTML;
+
+    if (val == "=") {
+      string = eval(string);
+      display.value = string;
+    } else if (val == "C") {
+      string = "";
+      display.value = string;
+    } else {
+      string = string + val;
+      display.value = string;
+    }
+  });
+});
